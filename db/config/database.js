81378b5aa2ed3a08c06a95600b2370224ee9c4d6
Aleksey Mityukov <imondoro@gmail.com>
2021-12-29 22:13:25 +0300

    first character

--- db/config/database.js
+++ db/config/database.js
@@ -23,7 +23,6 @@ module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         dialect: process.env.DB_HOST,
-        // use_env_variable: "DATABASE_URL",
         dialectOptions: {
             ssl: {rejectUnauthorized: false},
         },
--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -5,10 +5,9 @@ router.get('/', async (req, res) => {
         res.json({message: 'OK'})
     }
 );
-router.get('/get-user',
-    async (req, res) => {
-        const user = await Character.findOne({where: {id: 1}})
-        res.json(user)
-    })
+router.get('/get-user', async (req, res) => {
+    const user = await Character.findAll()
+    res.json(user)
+})
 
 module.exports = router
