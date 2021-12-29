6e1beee482ab46df26899b2514a13e1650adb14c
Aleksey Mityukov <imondoro@gmail.com>
2021-12-29 21:24:34 +0300

    figthing with heroku & sequelize

--- server.js
+++ server.js
@@ -8,6 +8,27 @@ const cors = require('cors');
 const FileStore = require('session-file-store')(session);
 // const { idAndName, userCheck } = require('./middlewares/middleware');
 
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
+);
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
+
+
 const router = require('./routes/indexRouter');
 // const userRouter = require('./routes/user');
 // const registerRouter = require('./routes/register');
