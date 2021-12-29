a2e76b643b15fa76bbeeae5b4d3f84925045556d
Aleksey Mityukov <imondoro@gmail.com>
2021-12-29 22:55:25 +0300

    fighting with heroku

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
 
-// const Sequelize = require('sequelize');
-// sequelize = new Sequelize(process.env.DATABASE_URL, {
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//                 rejectUnauthorized: false
-//             }
-//         }
-//     }
-// );
-//
-// sequelize
-//     .authenticate()
-//     .then(() => {
-//         console.log('Connection has been established successfully.');
-//     })
-//     .catch(err => {
-//         console.error('Unable to connect to the database:', err);
-//     });
+const Sequelize = require('sequelize');
+sequelize = new Sequelize(process.env.DATABASE_URL, {
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
+    }
+);
+
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err);
+    });
 
 
 const router = require('./routes/indexRouter');
