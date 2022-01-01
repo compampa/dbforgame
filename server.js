62163671da51b41b3b5153601b6514a5961e3e2e
Aleksey Mityukov <imondoro@gmail.com>
2022-01-01 14:19:36 +0300

    check db connection

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ const router = require('./routes/indexRouter');
 
 const app = express();
 
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 3000; // process.env.PORT ??
 
 // app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
