462345bafa74ad9f8ccd9b01da1ea5b7e8f81457
Aleksey Mityukov <imondoro@gmail.com>
2022-01-01 17:59:07 +0300

    added handlers for characters and classes

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,13 +1,21 @@
 const router = require('express').Router();
-const {Character} = require('../db/models')
+const {Character, Class, User} = require('../db/models')
 
 router.get('/', async (req, res) => {
         res.json({message: 'OK'})
     }
 );
 router.get('/get-user', async (req, res) => {
-    const user = await Character.findAll()
+    const user = await User.findAll()
     res.json(user)
+})
+router.get('/get-character', async (req, res) => {
+    const character = await Character.findAll()
+    res.json(character)
+})
+router.get('/get-classes', async (req, res) => {
+    const classes = await Class.findAll()
+    res.json(classes)
 })
 
 module.exports = router
