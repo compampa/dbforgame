e01f5dce0b1939106dd8af4e000631d18ca2b272
Aleksey Mityukov <imondoro@gmail.com>
2022-01-09 13:02:45 +0300

    0.02v

--- server.js
+++ server.js
@@ -23,12 +23,13 @@ app.use(cookieParser());
 
 
 app.use('/db', router);
-app.use('/api', index)
 app.get('/*', (req, res) => {
     // res.sendFile(path.join((__dirname, 'build', 'index.html')))
     res.sendFile('./build/index.html', {root: __dirname})
 
 })
+app.use('/api', index)
+
 app.use(errorMiddleware)
 
 app.listen(PORT, () => {
