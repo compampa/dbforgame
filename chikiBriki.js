12a10d304820fe679c510e9f202c4c8fd85437d7
Aleksey Mityukov <imondoro@gmail.com>
2022-01-15 16:13:48 +0300

    pushing...

--- chikiBriki.js
+++ chikiBriki.js
@@ -1,5 +1,5 @@
 const server = require('./sockets')
 
-const PORT = 8000 //process.env.PORT ||
+const PORT = process.env.PORT || 8000 //process.env.PORT ||
 
 server.listen(PORT, ()=> console.log(`OMFG it's happening on port ${PORT}`))
