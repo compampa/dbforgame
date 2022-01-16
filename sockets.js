5cecd7767e21a6adffdf09a2688df4440b189996
Aleksey Mityukov <imondoro@gmail.com>
2022-01-16 21:49:03 +0300

    rooms

--- sockets.js
+++ sockets.js
@@ -11,7 +11,7 @@ const io = new Server(server, {cors: {origin: true}})
 // const players = []
 
 io.on('connection', socket => {
-    const arr = []
+
     socket.on('message', ({name, message}) => {
         io.emit('message', {name, message})
     })
@@ -19,16 +19,17 @@ io.on('connection', socket => {
     socket.on('join-room', async (room, player, battlePlayer) => {
         try{
         socket.join(room.id)
+            const arr = []
             const currentRoom = await BattleRoom.findOne({where: {id: Number(room.id)}})
 
             if (Number(currentRoom.initial_character_id) === Number(player.id)){
                 arr.push({player,battlePlayer})
-                return io.to(room.id).emit('join-room', arr)
+                io.to(room.id).emit('join-room', arr)
             } else {
                 arr.push({player,battlePlayer})
                 await BattleRoom.update({opponent_id: Number(player.id), description: 'active'}, {where: {id: room.id}})
                 const currentRoom2 = await BattleRoom.findOne({where: {id: Number(room.id)}})
-                return io.to(room.id).emit('join-room', arr)
+                io.to(room.id).emit('join-room', arr)
             }
                  // await BattleRoom.update({opponent_id: Number(player.id), description: 'active'}, {where: {id: room.id}})
         } catch (e) {
