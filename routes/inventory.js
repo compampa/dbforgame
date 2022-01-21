e46c01cfb7bdda073b486f6d1a12d6350daf16de
Aleksey Mityukov <imondoro@gmail.com>
2022-01-21 12:14:42 +0300

    merchant

--- routes/inventory.js
+++ routes/inventory.js
@@ -35,6 +35,7 @@ router.post('/add-item', async (req,res)=> {
     console.log(req.body)
     const {player, item} = req.body
     try {
+        await Character.update({balance: player.balance - item.price }, {where: {id: player.id}})
         await Inventory.create({item_id: Number(item.id), character_id: Number(player.id)})
         res.sendStatus(200)
     } catch (e) {
